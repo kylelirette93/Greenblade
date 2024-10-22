ffb78332451a80c9d3fe12e39f94e47b34ca41f4
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2024-10-22 17:36:56 -0300

    Created a player controller, added some logic.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,5 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public float moveSpeed = 10f;
+    Animator anim;
+    Rigidbody2D rb;
+    bool facingRight = true;
+    float horizontalInput;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+
+        if (facingRight && horizontalInput < 0)
+        {
+            Flip();
+        }
+        else if (!facingRight && horizontalInput > 0)
+        {
+            Flip();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
