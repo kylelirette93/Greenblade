947292f50cbb0399c4ac295c32742af89fd02025
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2024-10-26 17:33:48 -0300

    Fixed pixel issue, added jump, added more of map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,24 +4,60 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Groundcheck and movement variables.
+    public float groundCheckDistance;
+    public LayerMask groundLayer;
+    public LayerMask stairLayer;
+    bool isGrounded;
+    bool facingRight = true;
     public float moveSpeed = 10f;
+    float horizontalInput;
+    float decelForce = 120f;
+
+    // Jump variables.
+    public float jumpForce = 20f;
+    bool isJumping = false;
+
+    // References.
     Animator anim;
     Rigidbody2D rb;
-    bool facingRight = true;
-    float horizontalInput;
+    BoxCollider2D collider;
 
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    void Jump()
+    {
+        if (isGrounded)
+        {
+            isGrounded = false;
+            isJumping = true;
+            anim.SetBool("isJumping", true);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 
 
 
     void Update()
     {
+        isGrounded = GroundCheck();
+
+        // Get horizontal input from player.
         horizontalInput = Input.GetAxis("Horizontal");
+
+        // Set walk speed parameter to match the velocity of player.
         anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
 
         if (facingRight && horizontalInput < 0)
         {
@@ -33,9 +69,47 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    bool GroundCheck()
+    {
+        Vector2 origin = new Vector2(collider.bounds.center.x, collider.bounds.min.y);
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, groundCheckDistance, groundLayer);
+
+        return hit.collider != null;
+    }
+
+    void StairCheck()
+    {
+        Vector2 origin = new Vector2(collider.bounds.center.x, collider.bounds.min.y);
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, groundCheckDistance, groundLayer);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.CompareTag("Stairs"))
+            {
+                if (rb.velocity.y > 0)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, 0);
+                }
+                else if (rb.velocity.y <= 0)
+                {
+                    rb.AddForce(Vector2.down * decelForce, ForceMode2D.Force);
+                }
+            }
+        }
+        
+    }
+
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        float verticalVelocity = rb.velocity.y;
+        StairCheck();
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, verticalVelocity);
+
+        if (isGrounded && rb.velocity.y <= 0)
+        {
+            isJumping = false;
+            anim.SetBool("isJumping", false);
+        }
     }
     void Flip()
     {
